2a5dcd3954fce9a87e363360610f40e0865a714c
lvqiya <1210806488@qq.com>
2018-11-26 11:20:01 +0800

    登录成功async与awiat

--- src/chajian/http.js
+++ src/chajian/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const MyHttpServer = {}
-MyHttpServer.install = (Vue)=>{
-   
+MyHttpServer.install = (Vue)=> {
+    axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
     // 4. 添加实例方法
     Vue.prototype.$http = axios
   }
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -8,7 +8,7 @@
             <el-form-item label="活动区域">
                 <el-input v-model="formdata.password"></el-input>
             </el-form-item>
-            <el-button type="primary" plain class="login_btn">登录</el-button>
+            <el-button type="primary" plain class="login_btn" @click.prevent="handelLogin()">登录</el-button>
         </el-form>
     </div>
 
@@ -23,8 +23,40 @@ export default {
         password: ""
       }
     };
+  },
+  methods:{
+      async handelLogin(){
+        //   不想在异步操作.then中获取结果
+          const res = await this.$http.post('login',this.formdata)
+          
+            //   console.log(res);
+              const{meta:{status,msg},data} = res.data
+              if (status === 200) {
+                  this.$router.push({name:'home'})
+                  this.$message.success(msg)
+              }else{
+                  this.$message.error(msg)
+              }
+              
+          }
+      }
+    //   handelLogin(){
+    //     //   不想在异步操作.then中获取结果
+    //       this.$http.post('login',this.formdata)
+    //       .then((res)=>{
+    //         //   console.log(res);
+    //           const{meta:{status,msg},data} = res.data
+    //           if (status === 200) {
+    //               this.$router.push({name:'home'})
+    //               this.$message.success(msg)
+    //           }else{
+    //               this.$message.error(msg)
+    //           }
+              
+    //       })
+    //   }
   }
-};
+;
 </script>
 
 <style>
