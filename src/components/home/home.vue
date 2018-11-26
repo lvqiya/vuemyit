cb2747b734ae9c446b764e5aa476438c76bbf039
lvqiya <1210806488@qq.com>
2018-11-26 17:58:10 +0800

    用户渲染

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -89,7 +89,9 @@
 
                 </el-menu>
             </el-aside>
-            <el-main class="main">Main</el-main>
+            <el-main class="main">
+                <router-view></router-view>
+            </el-main>
         </el-container>
     </el-container>
 </template>
