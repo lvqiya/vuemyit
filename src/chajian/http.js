4a61070081d358c003fc8d20f665de8d88d5baad
lvqiya <1210806488@qq.com>
2018-11-27 15:28:31 +0800

    又一次修改

--- src/chajian/http.js
+++ src/chajian/http.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 const MyHttpServer = {}
 MyHttpServer.install = (Vue)=> {
     axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
+    // 设置请求头 进行设置
+    
     // 4. 添加实例方法
     Vue.prototype.$http = axios
   }
