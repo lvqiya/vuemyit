ccd91b3dbf695ea77740dcd852294487168b443b
lvqiya <1210806488@qq.com>
2018-11-27 12:14:25 +0800

    添加用户

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -7,10 +7,10 @@
     </el-breadcrumb>
     <el-row class="searchArea">
       <el-col :span="24">
-        <el-input class="searchInput" clearable placeholder="请输入内容" v-model="query">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-input class="searchInput" clearable placeholder="请输入内容" v-model="query" @clear=loadData()>
+          <el-button slot="append" icon="el-icon-search" @click="searchUsers()" clearable></el-button>
         </el-input>
-        <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" plain @click="showAdd()">添加用户</el-button>
       </el-col>
     </el-row>
     <el-table :data="list" style="width: 100%">
@@ -24,9 +24,9 @@
       <el-table-column prop="mobile" label="电话" width="180">
       </el-table-column>
       <el-table-column label="创建日期" width="100">
-             <template slot-scope="list">
-               {{list.row.create_time | fmtdate}}
-             </template>    
+        <template slot-scope="list">
+          {{list.row.create_time | fmtdate}}
+        </template>
       </el-table-column>
       <el-table-column label="用户状态">
         <template slot-scope="scope">
@@ -37,12 +37,38 @@
       </el-table-column>
       <el-table-column label="操作" width="300">
         <template slot-scope="scope">
-          <el-button icon="el-icon-search" circle></el-button>
-          <el-button type="danger" icon="el-icon-delete" circle></el-button>
-          <el-button type="primary" icon="el-icon-edit" circle></el-button>
+          <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+
+          <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
         </template>
       </el-table-column>
     </el-table>
+
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagenum" :page-sizes="[2, 4, 6, 8]" :page-size="pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total">
+    </el-pagination>
+
+    <!-- 添加对话框 -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+      <el-form :model="form">
+        <el-form-item label="用户名称" label-width=100>
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width=100>
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width=100>
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width=100>
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click=Adduser()>确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -51,15 +77,26 @@ export default {
   data() {
     return {
       list: [
-        {create_time: ""},
-        {email: ""},
-        {id: ""},
-        {mg_state: ""},
-        {mobile: ""},
-        {role_name: ""},
-        {username: ""}
+        { create_time: "" },
+        { email: "" },
+        { id: "" },
+        { mg_state: "" },
+        { mobile: "" },
+        { role_name: "" },
+        { username: "" }
       ],
-      query: ""
+      query: "",
+      pagenum: 1,
+      pagesize: 2,
+      total: 10,
+      // 对话框添加
+      dialogFormVisibleAdd: false,
+      form: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      }
     };
   },
   mounted() {
@@ -67,11 +104,38 @@ export default {
   },
 
   methods: {
+    async Adduser() {
+      const res = await this.$http.post('/users',this.form)
+      console.log(res);
+      const {meta:{status,msg},data} = res.data
+      if (status === 200) {
+        this.dialogFormVisibleAdd = false
+        
+        this.$message.success(msg)
+        this.loadData()
+        for (const key in this.form) {
+          this.form[key] = ''
+        }
+        
+      }else{
+        this.$message.error(msg)
+      }
+      
+    },
+    showAdd() {
+      this.dialogFormVisibleAdd = true;
+    },
+    searchUsers() {
+      this.loadData();
+      this.pagenum = 1;
+    },
     async loadData() {
       const AUTH_TOKEN = localStorage.getItem("token");
       this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
       const res = await this.$http.get(
-        `/users?query=${this.query}&pagenum=1&pagesize=10 `
+        `/users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
+          this.pagesize
+        } `
       );
       // {
       //   headers: { Authorization: window.localStorage.getItem("token") },
@@ -92,11 +156,24 @@ export default {
         const {
           data: { users }
         } = data;
-        this.total = total
+        this.total = total;
         this.list = users;
       } else {
         this.$message.error(msg);
       }
+    },
+    handleSizeChange(val) {
+      // 条数发生变化
+      console.log(`每页 ${val} 条`);
+      this.pagesize = val;
+      this.loadData();
+      this.pagenum = 1;
+    },
+    handleCurrentChange(val) {
+      // 页码发生变化
+      console.log(`当前页: ${val}`);
+      this.pagenum = val;
+      this.loadData();
     }
   }
 };
