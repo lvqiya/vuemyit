1f4081e2637e5e553edfdfae9964e2d645ba0709
lvqiya <1210806488@qq.com>
2018-11-27 10:13:20 +0800

    修改数据列表

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,72 +1,90 @@
 <template>
-    <el-card class="box-card">
-        <el-breadcrumb separator="/">
-            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
-            <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-        </el-breadcrumb>
-        <el-row class="searchArea">
-            <el-col :span="24">
-                <el-input class="searchInput" clearable placeholder="请输入内容">
-                    <el-button slot="append" icon="el-icon-search"></el-button>
-                </el-input>
-                <el-button type="success" plain>添加用户</el-button>
-            </el-col>
-        </el-row>
-        <el-table :data="list" style="width: 100%">
-            <!-- 序号 -->
-            <el-table-column type="index" width="50">
-            </el-table-column>
-            <el-table-column prop="date" label="姓名" width="180">
-            </el-table-column>
-            <el-table-column prop="name" label="邮箱" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="电话" width="180">
-            </el-table-column>
-            <el-table-column prop="mobile" label="创建日期" width="100">
+  <el-card class="box-card">
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row class="searchArea">
+      <el-col :span="24">
+        <el-input class="searchInput" clearable placeholder="请输入内容" v-model="query">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+    <el-table :data="list" style="width: 100%">
+      <!-- 序号 -->
+      <el-table-column type="index" width="50">
+      </el-table-column>
+      <el-table-column prop="username" label="姓名" width="100">
+      </el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180">
+      </el-table-column>
+      <el-table-column prop="mobile" label="电话" width="180">
+      </el-table-column>
+      <el-table-column prop="create_time" label="创建日期" width="100">
 
-            </el-table-column>
-            <el-table-column label="用户状态">
-                <template slot-scope="scope">
-                    <!-- scope.row就是当前绑定的数据对象 -->
-                    <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
-                    </el-switch>
-                </template>
-            </el-table-column>
-            <el-table-column label="操作" width="300">
-                <template slot-scope="scope">
-                    <el-button icon="el-icon-search" circle></el-button>
-                    <el-button type="danger" icon="el-icon-delete" circle></el-button>
-                    <el-button type="primary" icon="el-icon-edit" circle></el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-    </el-card>
+      </el-table-column>
+      <el-table-column label="用户状态">
+        <template slot-scope="scope">
+          <!-- scope.row就是当前绑定的数据对象 -->
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="300">
+        <template slot-scope="scope">
+          <el-button icon="el-icon-search" circle></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button type="primary" icon="el-icon-edit" circle></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      list: []
+      list: [
+        {create_time: ""},
+        {email: ""},
+        {id: ""},
+        {mg_state: ""},
+        {mobile: ""},
+        {role_name: ""},
+        {username: ""}
+      ],
+      query: ""
     };
   },
   mounted() {
     this.loadData();
   },
+
   methods: {
     async loadData() {
-      const res = await this.$http.get(`/users`, {
-        headers: { Authorization: window.localStorage.getItem("token") },
+      const AUTH_TOKEN = localStorage.getItem("token");
+      this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+      const res = await this.$http.get(
+        `/users?query=${this.query}&pagenum=1&pagesize=10 `
+      );
+      // {
+      //   headers: { Authorization: window.localStorage.getItem("token") },
 
-        params: {
-          pagenum: 1,
-          pagesize: 10
-        }
-      });
+      //   params: {
+      //     pagenum: 1,
+      //     pagesize: 10
+      //   }
+      // });
       const data = res.data;
+      console.log(res);
+
       const {
-        meta: { msg, status }
+        meta: { msg, status },
+        data: { total, users }
       } = data;
       if (status === 200) {
         const {
