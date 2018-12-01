e411454e9e0b4d63b6ba91d3370599daea14e2e1
lvqiya <1210806488@qq.com>
2018-12-01 12:10:39 +0800

    组件四图片暂时完成

--- src/components/goods/goodsadd.vue
+++ src/components/goods/goodsadd.vue
@@ -1,159 +1,205 @@
 <template>
-<el-card class="box-card">
+  <el-card class="box-card">
     <!-- 面包屑 -->
-    <my-bread level1='商品管理' level2='商品列表'></my-bread>
+    <my-bread level1="商品管理" level2="商品列表"></my-bread>
     <!-- 提示框 -->
     <el-alert class="alert" title="添加商品信息" type="info" center :closable="false" show-icon></el-alert>
     <!-- 步骤条 -->
     <el-steps :active="active-0" finish-status="success" simple style="margin-top: 20px">
-        <el-step title="基本信息"></el-step>
-        <el-step title="商品参数"></el-step>
-        <el-step title="商品属性"></el-step>
-        <el-step title="商品图片"></el-step>
-        <el-step title="商品内容"></el-step>
+      <el-step title="基本信息"></el-step>
+      <el-step title="商品参数"></el-step>
+      <el-step title="商品属性"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
     </el-steps>
     <!-- tab切换 -->
-    <el-form class="form" label-position="top" :model="form" label-width="80px" style="height: 350px;">
-        <!-- 为了提交所有信息添加form -->
-        <el-tabs class="tabs" v-model="active" tab-position="left" @tab-click="tabchange()">
-            <el-tab-pane name="1" label="基本信息">基本信息
-                <el-form-item label="商品名称">
-                    <el-input v-model="form.goods_name"></el-input>
-                </el-form-item>
-                <el-form-item label="商品价格">
-                    <el-input v-model="form.goods_price"></el-input>
-                </el-form-item>
-                <el-form-item label="商品重量">
-                    <el-input v-model="form.goods_weight"></el-input>
-                </el-form-item>
-                <el-form-item label="商品数量">
-                    <el-input v-model="form.goods_number"></el-input>
-                </el-form-item>
-                <!-- 级联选择器 -->
-                <!-- :props:{label,value,children}数据来源于option绑定的数据名 -->
-                <el-form-item label="商品数量">
-                    <el-cascader expand-trigger="hover" :options="options" v-model="selectedOptions" @change="handleChange" :props="defaultProps">
-                    </el-cascader>
-                </el-form-item>
+    <el-form
+      class="form"
+      label-position="top"
+      :model="form"
+      label-width="80px"
+      style="height: 350px;"
+    >
+      <!-- 为了提交所有信息添加form -->
+      <el-tabs class="tabs" v-model="active" tab-position="left" @tab-click="tabchange()">
+        <el-tab-pane name="1" label="基本信息">基本信息
+          <el-form-item label="商品名称">
+            <el-input v-model="form.goods_name"></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格">
+            <el-input v-model="form.goods_price"></el-input>
+          </el-form-item>
+          <el-form-item label="商品重量">
+            <el-input v-model="form.goods_weight"></el-input>
+          </el-form-item>
+          <el-form-item label="商品数量">
+            <el-input v-model="form.goods_number"></el-input>
+          </el-form-item>
+          <!-- 级联选择器 -->
+          <!-- :props:{label,value,children}数据来源于option绑定的数据名 -->
+          <el-form-item label="商品数量">
+            <el-cascader
+              expand-trigger="hover"
+              :options="options"
+              v-model="selectedOptions"
+              @change="handleChange"
+              :props="defaultProps"
+            ></el-cascader>
+          </el-form-item>
+        </el-tab-pane>
 
-            </el-tab-pane>
+        <el-tab-pane name="2" label="商品参数">商品参数
+          <!-- 点击二tab获取数据 -->
+          <el-form-item v-for="item in dynamicsParams" :key="item.attr_id" :label="item.attr_name">
+            <el-checkbox-group v-model="item.attr_vals">
+              <el-checkbox border v-for="item2 in item.attr_vals" :key="item2" :label="item2"></el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+        </el-tab-pane>
 
-            <el-tab-pane name="2" label="商品参数">商品参数
-                <!-- 点击二tab获取数据 -->
-                <el-form-item v-for="item in dynamicsParams" :key="item.attr_id" :label="item.attr_name">
-                    <el-checkbox-group v-model="item.attr_vals">
-                        <el-checkbox border v-for="item2 in item.attr_vals" :key="item2" :label="item2"></el-checkbox>
-                    </el-checkbox-group>
-                </el-form-item>
-            </el-tab-pane>
-
-            <el-tab-pane name="3" label="商品属性">商品属性
-                <el-form-item :label="item.attr_name" v-for="(item,i) in arrStaticparams" :key="i">
-                    <el-input v-model="item.attr_vals"></el-input>
-                </el-form-item>
-            </el-tab-pane>
-            <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
-            <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
-        </el-tabs>
+        <el-tab-pane name="3" label="商品属性">商品属性
+          <el-form-item :label="item.attr_name" v-for="(item,i) in arrStaticparams" :key="i">
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="4" label="商品图片">商品图片
+          <el-upload
+            action="http://127.0.0.1:8888/api/private/v1/upload"
+            :on-remove="handleRemove"
+            :on-success="handleSuccess"
+            :on-preview="handlePreview"
+            :headers="tokenHeader"
+            list-type="picture"
+          >
+            <el-button size="small" type="primary">点击上传</el-button>
+          </el-upload>
+        </el-tab-pane>
+        <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+      </el-tabs>
     </el-form>
-</el-card>
+  </el-card>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            active: "1",
-            form: {
-                // goods_name	商品名称	不能为空
-                // goods_cat	以为','分割的分类列表	不能为空
-                // goods_price	价格	不能为空
-                // goods_number	数量	不能为空
-                // goods_weight	重量	不能为空
-                // goods_introduce	介绍	可以为空
-                // pics	上传的图片临时路径（对象）	可以为空
-                // attrs	商品的参数（数组）
-                goods_name: '',
-                goods_cat: '',
-                goods_pric: '',
-                goods_numbe: '',
-                goods_pricer: '',
-                goods_weight: '',
-                goods_introduce: '',
-                pics: '',
-                attrs: '',
-            },
-            // 三级分类的数据
-            options: [],
+  data() {
+    return {
+      active: "1",
+      form: {
+        // goods_name	商品名称	不能为空
+        // goods_cat	以为','分割的分类列表	不能为空
+        // goods_price	价格	不能为空
+        // goods_number	数量	不能为空
+        // goods_weight	重量	不能为空
+        // goods_introduce	介绍	可以为空
+        // pics	上传的图片临时路径（对象）	可以为空
+        // attrs	商品的参数（数组）
+        goods_name: '',
+        goods_cat: '',
+        goods_pric: '',
+        goods_numbe: '',
+        goods_pricer: '',
+        goods_weight: '',
+        goods_introduce: '',
+        pics: '',
+        attrs: '',
+      },
+      // 三级分类的数据
+      options: [],
 
-            // v-model绑定的数据
-            selectedOptions: [],
-            defaultProps: {
-                value: 'cat_id',
-                label: 'cat_name',
-                children: 'children'
-            },
-            // 动态参数数据
-            dynamicsParams: [],
-            // 静态参数数据
-            arrStaticparams: []
+      // v-model绑定的数据
+      selectedOptions: [],
+      defaultProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      // 动态参数数据
+      dynamicsParams: [],
+      // 静态参数数据
+      arrStaticparams: [],
+      //   图片上传头部信息
+      tokenHeader:{Authorization:localStorage.getItem("token")}
 
-        }
-    },
-    created() {
-        this.getgoodslist()
-    },
-    methods: {
-        async tabchange() {
-            // console.log(1111);
-            if (this.selectedOptions.length !== 3) {
-                this.$message.warning('请先选择三级分类')
-                return
-            }
-            if (this.active === "2") {
-
-                const res = await this.$http.get(`categories/${this.selectedOptions[2]}/attributes?sel=many`)
-                // console.log(res);
-                const {
-                    data: resData
-                } = res
-                this.dynamicsParams = resData.data
-                // console.log(this.dynamicsParams);
-
-                this.dynamicsParams.forEach(item => {
-                    item.attr_vals = item.attr_vals.trim().length === 0 ? [] : item.attr_vals.trim().split(',')
-                });
-
-            } else if (this.active === "3") {
-                const res = await this.$http.get(`categories/${this.selectedOptions[2]}/attributes?sel=only`)
-                // console.log(res);
-                this.arrStaticparams = res.data.data
-
-            }
-
-        },
-        handleChange() {
-
-        },
-        async getgoodslist() {
-            const res = await this.$http.get(`categories?type=3`)
-            // console.log(res);
-            this.options = res.data.data
-            // console.log(this.options);
-
-        }
     }
+  },
+  created() {
+    this.getgoodslist()
+  },
+  methods: {
+      handlePreview(file){
+
+      },
+      handleRemove (file, fileList) {
+    //   const index = this.form.pics.findIndex((item) => {
+    //     return item.pic === file.response.data.tem_path
+    //   })
+    //   this.form.pics.splice(index, 1)
+    //   console.log(this.form)
+    },
+
+    handleSuccess (response, file, fileList) {
+    //   const { meta, data } = response
+
+    //   if (meta.status === 200) {
+    //     this.$message.success('图片上传成功')
+    //     this.form.pics.push({
+    //       pic: response.data.tmp_path
+    //     })
+    //   } else {
+    //     this.$message.error(meta.msg)
+    //   }
+    },
+    async tabchange() {
+      // console.log(1111);
+      if (this.selectedOptions.length !== 3) {
+        this.$message.warning('请先选择三级分类')
+        return
+      }
+      if (this.active === "2") {
+
+        const res = await this.$http.get(`categories/${this.selectedOptions[2]}/attributes?sel=many`)
+        // console.log(res);
+        const {
+          data: resData
+        } = res
+        this.dynamicsParams = resData.data
+        // console.log(this.dynamicsParams);
+
+        this.dynamicsParams.forEach(item => {
+          item.attr_vals = item.attr_vals.trim().length === 0 ? [] : item.attr_vals.trim().split(',')
+        });
+
+      } else if (this.active === "3") {
+        const res = await this.$http.get(`categories/${this.selectedOptions[2]}/attributes?sel=only`)
+        // console.log(res);
+        this.arrStaticparams = res.data.data
+
+      }
+
+    },
+    handleChange() {
+
+    },
+    async getgoodslist() {
+      const res = await this.$http.get(`categories?type=3`)
+      // console.log(res);
+      this.options = res.data.data
+      // console.log(this.options);
+
+    }
+  }
 }
 </script>
 
 <style>
 .alert {
-    margin-top: 26px;
-    margin-bottom: 10px;
+  margin-top: 26px;
+  margin-bottom: 10px;
 }
 
 .form {
-    height: 400px;
-    overflow: auto;
+  height: 400px;
+  overflow: auto;
 }
 </style>
